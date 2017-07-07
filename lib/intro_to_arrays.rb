15608b5afe3588e7a23e5b675384d862690565d4
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-07 16:57:53 -0400

    array22

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+instantiate_new_array = []
+ @my_new_array = instantiate_new_array
+ end
+
+def array_with_two_elements
+    pets = ["Shelby", "Purdy"]
+end
+
+
+def first_element (taylor_swift)
+    @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+     my_first_element = taylor_swift[0]
+end
+
+def third_element (taylor_swift)
+    my_third_element = @taylor_swift[2]
+end
+
+def last_element (taylor_swift)
+my_last_element = @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_country)
+    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    first_country = @south_east_asia.first
+    end
+
+def last_element_with_array_methods(first_country)
+    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    first_country = @south_east_asia.last
+end
+
+def length_of_array (programming_languages)
+@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+@programming_languages.length
+end
